6c474f35e99d8e5d4b2aa8b0d798928968559790
bruh00000 <lookpee0911@gmail.com>
2025-11-21 10:23:23 +0700

    update2

--- bin/dart_week5.dart
+++ bin/dart_week5.dart
@@ -1,20 +1,5 @@
-import 'dart:io';
-
-void add(int num1,int num2){
-  int sum = num1 + num2;
-  print("The sum is: $sum");
-}
-
-void printName(){
-  String fullName = "Mark Zukerburg";
-  print("My name is $fullName");
-}
+import 'function_call.dart';
 
 void main(){
-  print("Enter num");
-  int nums1 = int.parse(stdin.readLineSync()!);
-  int nums2 = int.parse(stdin.readLineSync()!);
-  
   printName();
-  add(nums1, nums2);
 }
